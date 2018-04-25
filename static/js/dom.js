1289a633b7f270c3a922f93a2e0589001778c06b
Pawel Krupa <pawel.krupa.junior@gmail.com>
2018-04-25 15:00:25 +0200

    Display of boards list + toogle board view

--- static/js/dom.js
+++ static/js/dom.js
@@ -2,20 +2,56 @@
 let dom = {
     loadBoards: function() {
         // retrieves boards and makes showBoards called
-        this.showBoards(dataHandler.getBoards());
+        let boards = dataHandler.getBoards();
+        setTimeout(function() {
+            document.getElementById("loading").style.display = "none";
+            dom.showBoards(boards);
+        }, 500);
     },
     showBoards: function(boards) {
         // shows boards appending them to #boards div
         // it adds necessary event listeners also
-        let div = document.getElementById("boards");
-        
+        let boardsListContainer = document.getElementById("boards");
+
         boards.forEach(board => {
-            let button = document.createElement("BUTTON");
-            let text = document.createTextNode(board.title); 
-            button.appendChild(text);
-            button.id = "board " + board.id;
+            let boardListElement = document.createElement("div");
+            boardListElement.className = "board-container";
+            boardListElement.id = "board-" + board.id;
+            let boardBar = document.createElement("div");
+            boardBar.className = "board-bar";
+            let boardHeader = document.createElement("h3");
+            let boardTitle = document.createTextNode(board.title);
+            let dropIcon = document.createElement("i");
+            dropIcon.className = "fas fa-caret-down fa-2x";
             
-            div.appendChild(button);
+            boardsListContainer.appendChild(boardListElement);
+            boardListElement.appendChild(boardBar);
+            boardBar.appendChild(boardHeader);
+            boardBar.appendChild(dropIcon);
+            boardHeader.appendChild(boardTitle);
+
+            let boardDetails = document.createElement("div");
+            boardDetails.className = "board-details";
+            boardListElement.appendChild(boardDetails);
+            
+            let statusesContainer = document.createElement("div");
+            statusesContainer.className = "row";
+            boardDetails.appendChild(statusesContainer);
+
+            let statuses = dataHandler.getStatuses();
+
+            statuses.forEach(status => {
+                let stausHeader = document.createElement("div");
+                stausHeader.id = "status-" + status.id;
+                stausHeader.className = "col-3 status-title";
+                let statusTitle = document.createTextNode(status.name);
+                statusesContainer.appendChild(stausHeader);
+                stausHeader.appendChild(statusTitle);
+            });
+
+            boardBar.addEventListener("click", function() {
+                dom.toggleViewBoard(board.id);
+            });
         });
     },
     loadCards: function(boardId) {
@@ -27,6 +63,16 @@ let dom = {
         // it adds necessary event listeners also
     },
     // here comes more features
-    ShowBoard: function(boardId) {
-    }
-}
+    toggleViewBoard: function(boardId) {
+        let board = dataHandler.getBoard(boardId);
+        let boardContainer = document.getElementById("board-" + board.id);
+        let boardDetails = boardContainer.getElementsByClassName("board-details")[0];
+        board.is_active = !board.is_active;
+        boardContainer.classList.toggle("open");
+        if (board.is_active) {
+            boardDetails.style.display = "block";
+        } else {
+            boardDetails.style.display = "none";
+        }
+    },
+};
