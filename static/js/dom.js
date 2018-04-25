d301a5a069ab10a528f71a619e2e523f4d6527f5
Witold Tafel <witold.tafel@gmail.com>
2018-04-25 14:08:18 +0200

    loading and showing cards, not tested

--- static/js/dom.js
+++ static/js/dom.js
@@ -13,18 +13,28 @@ let dom = {
             let button = document.createElement("BUTTON");
             let text = document.createTextNode(board.title); 
             button.appendChild(text);
-            button.id = "board " + board.id;
+            button.id = "board" + board.id;
             
             div.appendChild(button);
         });
     },
     loadCards: function(boardId) {
         // retrieves cards and makes showCards called
-        
+        this.showCards(dataHandler.getCardsByBoardId(boardId));
     },
     showCards: function(cards) {
         // shows the cards of a board
         // it adds necessary event listeners also
+        let status = document.getElementById("statuses").lastChild;
+        
+        cards.forEach(card => {
+            let div = document.createElement("div");
+            let text = document.createTextNode(card.title); 
+            div.appendChild(text);
+            div.id = "card" + card.id;
+            
+            status.appendChild(div);
+        });
     },
     // here comes more features
     ShowBoard: function(boardId) {
