cba66a7a80fa5986857319b0a418c2224cd9f538
Pawel Krupa <pawel.krupa.junior@gmail.com>
2018-05-15 12:32:41 +0200

    updated naming convention

--- Proman/Trollo/database.sql
+++ Proman/Trollo/database.sql
@@ -16,32 +16,32 @@ CREATE TABLE statuses (
 
 CREATE TABLE cards (
     id serial NOT NULL PRIMARY KEY,
-    table_id integer,
+    board_id integer,
     status_id integer,
     name text
 );
 
 CREATE TABLE boards_statuses (
-    table_id integer,
+    board_id integer,
     status_id integer
 );
 
 CREATE TABLE boards_accounts (
-    user_id integer,
-    table_id integer
+    account_id integer,
+    board_id integer
 );
 
 ALTER TABLE ONLY cards
-    ADD CONSTRAINT fk_table_id FOREIGN KEY (table_id) REFERENCES boards(id) ON DELETE CASCADE;
+    ADD CONSTRAINT fk_board_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE;
 ALTER TABLE ONLY cards
     ADD CONSTRAINT fk_status_id FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY boards_statuses
-    ADD CONSTRAINT fk_user_id FOREIGN KEY (table_id) REFERENCES boards(id) ON DELETE CASCADE;
+    ADD CONSTRAINT fk_account_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE;
 ALTER TABLE ONLY cards
     ADD CONSTRAINT fk_statuses_id FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY boards_accounts
-    ADD CONSTRAINT fk_accounts_id FOREIGN KEY (user_id) REFERENCES accounts(id) ON DELETE CASCADE;
+    ADD CONSTRAINT fk_accounts_id FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE;
 ALTER TABLE ONLY cards
-    ADD CONSTRAINT fk_boards_id FOREIGN KEY (table_id) REFERENCES boards(id) ON DELETE CASCADE;
+    ADD CONSTRAINT fk_boards_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE;
