fba75956dcc257aea3c82c3862657cea914d4b6f
Matixk <matixk73@gmail.com>
2018-05-15 12:10:52 +0200

    Database connector, data manager with login and registration, server routing for login, registration and logout

---
+++ data_manager.py
@@ -0,0 +1,38 @@
+import database_connector
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+@database_connector.connection_handler
+def is_user_in_database(cursor, username):
+    cursor.execute("""
+                    SELECT username FROM users
+                    WHERE LOWER(username) = LOWER(%(username)s);
+                   """,
+                   {'username': username})
+    if(cursor.fetchall()):
+        return True
+    return False
+
+
+@database_connector.connection_handler
+def add_user(cursor, username, password):
+    cursor.execute("""
+                    INSERT INTO users (username, password)
+                    VALUES (%(username)s, %(password)s);
+                   """,
+                   {'username': username, 'password': generate_password_hash(password)})
+
+
+@database_connector.connection_handler
+def signin(cursor, username, password):
+    cursor.execute("""
+                    SELECT username, password FROM users
+                    WHERE LOWER(username) = LOWER(%(username)s);
+                   """,
+                   {'username': username})
+    db_user = cursor.fetchall()[0]
+
+    if (check_password_hash(db_user['password'], password)):
+        return db_user['username']
+    else:
+        return None
